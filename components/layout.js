0a6e8abcb5c561235ba5336602118154a5365521
Adrian Lee <jun.irok@gmail.com>
2020-07-26 14:04:32 -0700

    Add goat counter

--- components/layout.js
+++ components/layout.js
@@ -23,6 +23,11 @@ export default function Layout({ children, home }) {
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <script
+          data-goatcounter="https://adrianlee.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
       </Head>
       <header className={styles.header}>
         {false && home ? (
